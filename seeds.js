6265adaeef334db7fd40e511cffb2feef3039a76
calebj4 <calebjohnson@Calebs-Laptop.local>
2023-04-11 16:47:29 -0400

    MVP Reached now we style

--- seeds.js
+++ seeds.js
@@ -15,39 +15,39 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 
 const productArr = [
   {
-    name: 'Jordan 1 x Union LA',
-    description: 'Lights, Cameras, Action! Nike has officially gone Hollywood and released the Jordan 1 Retro High Union Los Angeles Black Toe. This unique Jordan 1 comes with a white leather upper with a red accent, black Nike "Swoosh", cream midsole, and red sole.',
-    img: 'https://i.ebayimg.com/images/g/ckMAAOSwv-xb8FLT/s-l1600.jpg',
-    price: 190,
-    qty: 82
+    name: 'Rubber Soul by The Beatles',
+    description: 'Rubber Soul is the sixth studio album by the English rock band the Beatles. It was released on 3 December 1965 in the United Kingdom, on the Parlophone label, accompanied by the non-album double A-side single Day Tripper.'
+    ,img: 'https://i.discogs.com/XO-Barxecgb47E1_XVm2WYDRfgwPzxEgJV9JmEmNLKo/rs:fit/g:sm/q:90/h:593/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM5MzYx/Ny0xNTc3NjQyNTc5/LTgwMDQuanBlZw.jpeg',
+    price: 45,
+    qty: 376
   }, 
   {
-    name: 'Air Force One Low 07',
-    description: "The Nike Air Force 1 Low White 2007 features an all-white leather upper with a perforated toe box and Swoosh overlays. A Nike heel embroidery and white sole completes the design.",
-    img: 'https://m.media-amazon.com/images/I/81uiWMk9dnL._AC_UL1500_.jpg',
-    price: 110,
+    name: 'Igor by Tyler The Creator',
+    description: "Vinyl LP pressing in gatefold jacket. 2019 release. Igor is the fifth studio album by rapper Tyler, The Creator. Produced entirely by Tyler, the album follows the 2017 release of Flower Boy. It features guest appearances from Playboi Carti, Lil Uzi Vert, Solange, Kanye West, and Jerrod Carmichael, along with backing vocals from Santigold, Jessy Wilson, La Roux, Ceelo Green, Charlie Wilson, Slowthai, and Pharrell, among others.",
+    img: 'https://target.scene7.com/is/image/Target/GUEST_f31582c0-bf02-4543-9cc7-41b86430a92e?wid=488&hei=488&fmt=pjpeg',
+    price: 84,
     qty: 17
   }, {
-    name: 'Staple Pigeon Nike SB Dunk',
-    description: 'Kung Fu Panda would probably lose his mind after seeing the Nike SB Dunk Low Staple Panda Pigeon. This Nike SB comes in a white upper with black accents, white Nike "Swoosh", black midsole and translucent sole. ',
-    img: 'https://images.stockx.com/images/Nike-SB-Dunk-Low-Panda-Pigeon-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1607048105',
+    name: 'The Blueprint by Jay-Z',
+    description: 'The sixth studio album by American rapper Jay-Z, released on September 11, 2001, through Roc-A-Fella Records and Def Jam Recordings. The Blueprint features soul-based sampling and production handled primarily by Kanye West, Just Blaze, and Bink, as well as Timbaland, Trackmasters, and Eminem, who also contributes the only guest feature on the album.',
+    img: 'https://upload.wikimedia.org/wikipedia/en/2/2c/The_Blueprint.png',
     price: 850,
     qty: 8
   },   {
-      name: 'Nike Air Max 1 OG 86',
-      description: 'The Air Max 1 ‘86 Big Bubble released as Nikes premier Air Max Day release in 2023. The sneakers are constructed using grey nubuck, white mesh and red suede across the uppers and a large air unit on the midsole. Nike used Sport Red branding on the tongue and heel of the sneaker.',
-      img: 'https://media.endclothing.com/end-features/f_auto,q_auto:eco,w_1520/prodfeatures/df24a3c2-2246-452e-9b64-2bf9a8457f28_DQ3989-100_launches_hero_landscape_1.png?auto=compress,format',
-      price: 150,
+      name: 'Songs in The Key of Life by Stevie Wonder',
+      description: 'Songs in the Key of Life is the eighteenth studio album by American singer, songwriter and musician Stevie Wonder. A double album, it was released on September 28, 1976, by Tamla Records, a division of Motown. It was recorded primarily at Crystal Sound studio in Hollywood, with some sessions recorded at the Record Plant in Hollywood, the Record Plant in Sausalito, and The Hit Factory in New York City; final mixing was conducted at Crystal Sound. The album has been regarded by music journalists as the culmination of his classic period of releases.',
+      img: 'https://upload.wikimedia.org/wikipedia/en/e/e2/Songs_in_the_key_of_life.jpg',
+      price: 85,
       qty: 110
     }, {
-      name: 'New Balance 550',
-      description: "The New Balance 550 White Green features a smooth white leather upper with a grey suede toe wrap and green detailing. From there, a New Balance Basketball woven tongue label and matching sole completes the design.",
-      img: 'https://www.lesitedelasneaker.com/wp-content/images/2023/01/new-balance-550-beige-nightwatch-green-bb550vtc-banner.jpg',
-      price: 110,
-      qty: 17
+      name: 'Blonde by Frank Ocean',
+      description: "Blonde is the second studio album by the enigmatic Frank Ocean. The album was released on August 20, 2016, following a four-year hiatus after the release of channel ORANGE. In The Guardian review of the album, reviewer Tim Jonze called Blonde, “one of the most intriguing and contrary records ever made.” It is considered to be one of the greatest R&B/pop albums of all time.",
+      img: 'https://upload.wikimedia.org/wikipedia/en/a/a0/Blonde_-_Frank_Ocean.jpeg',
+      price: 500,
+      qty: 2
     }, {
-      name: 'Adidas Ultra Boost White Multi Color',
-      description: 'This Ultra Boost 4 is categorized under adidas running shoes. Its upper part is made of woven Primeknit that is complemented with multicolored yarns. An Ultra Boost branding can be seen on the back right side of the right shoe, with a similar branding spotted on the back left side of the left shoe. A white midsole sits on a black outsole to complete the look of this sneaker.',
+      name: 'Hiatus Kaiyote',
+      description: 'Mood Valiant is the third studio album by Australian neo-soul quartet Hiatus Kaiyote, released in 2021 on Brainfeeder. The album peaked at number 4 on the ARIA charts. At the 2021 ARIA Music Awards, the album was nominated for Best Soul/R&B Release.',
       img: 'https://images.stockx.com/images/adidas-Ultra-Boost-4-White-Multi-Color-White-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1610084262',
       price: 180,
       qty: 8
