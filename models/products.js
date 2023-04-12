4d87bba685f4be9e1a8082e5c8d59385af329b6a
calebj4 <calebjohnson@Calebs-Laptop.home>
2023-04-12 17:05:38 -0400

    changing show style

--- models/products.js
+++ models/products.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
 	name: { type: String, required: true },
-	description: { type: String, required: true },
+	artist: { type: String, required: true },
     img:  {type: String, required: true },
     price: {type: Number, required: true},
     qty: {type: Number, required: true},
